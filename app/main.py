d058dcefb06cedefeb852317f9b27d215417c17e
Alexandr <khsashko@yandex.ru>
2025-10-23 16:21:56 +0300

    TASK-437: Задача - Middleware (Логирование)

--- app/main.py
+++ app/main.py
@@ -1,10 +1,24 @@
 import asyncio
 
-from fastapi import FastAPI
+import datetime
+import logging
+
+from fastapi import FastAPI, Request
 
 from app.routers import books
 
 app = FastAPI()
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+@app.middleware("http")
+async def logs(request: Request, call_next):
+
+    logger.info(f"{request.method}, {request.url}, {datetime.datetime.now()}")
+    response = await call_next(request)
+    return response
 
 
 @app.get("/")
