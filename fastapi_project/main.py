d901fa79963bd1e48cef6232d86664f43e62cc7c
Alexandr <khsashko@yandex.ru>
2025-10-23 15:28:31 +0300

    TASK-437: Задача - CRUD-эндпоинты

--- fastapi_project/main.py
+++ fastapi_project/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 
+from books.models import BookScheme
 
 app = FastAPI()
 
@@ -7,3 +8,20 @@ app = FastAPI()
 @app.get("/")
 def hello():
     return {"message": "Hello, World"}
+
+
+books_db = {}
+current_id = 1
+
+
+@app.post("/books")
+def post_book(book: BookScheme):
+    global current_id
+    books_db[current_id] = book
+    current_id += 1
+    return {"id": current_id - 1, **book.dict()}
+
+
+@app.get("/books/{book_id}")
+def get_book(book_id: int):
+    return {"id": book_id, **books_db[book_id].dict()}
