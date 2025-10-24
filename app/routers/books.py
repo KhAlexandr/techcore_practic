94eb4973e86f963c6e6731f49fb25456b5210613
Alexandr <khsashko@yandex.ru>
2025-10-24 13:00:53 +0300

    TASK-439: Задача - Репозиторий (Async CRUD)

--- app/routers/books.py
+++ app/routers/books.py
@@ -2,35 +2,45 @@ import asyncio
 
 from fastapi import APIRouter, Depends, HTTPException
 
+from sqlalchemy import select
+
 from app.books.schemas import BookScheme
+from app.books.models import Book
+from app.database import session_maker
 
 
 router = APIRouter(prefix="/books", tags=["Упрпвление книгами"])
 
 
-books_db = {}
-current_id = 1
+class BookRepository:
+    async def get_by_id(self, book_id: int):
+        async with session_maker() as session:
+            result = await session.execute(select(Book).where(Book.id == book_id))
+            return result.scalar_one_or_none()
+
+    async def create(self, title: str, year: int | None = None) -> Book:
+        async with session_maker() as session:
+            book = Book(title=title, year=year)
+            session.add(book)
+            await session.commit()
+            await session.refresh(book)
+            return book
 
 
-def get_db_session():
-    session = books_db
-    yield session
+book_repo = BookRepository()
 
 
 @router.post("/")
-async def create_book(book: BookScheme, db: dict = Depends(get_db_session)):
-    global current_id
-    db[current_id] = book
-    current_id += 1
-    await asyncio.sleep(1)
-    return {"id": current_id - 1, **book.dict()}
+async def create_book(book: BookScheme):
+    new_book = await book_repo.create(title=book.title, year=book.year)
+    return {"id": new_book.id, "title": new_book.title, "year": new_book.year}
 
 
 @router.get("/{book_id}")
 async def get_book(book_id: int):
-    await asyncio.sleep(1)
-    if book_id not in books_db:
+    book = await book_repo.get_by_id(book_id)
+    if not book:
         raise HTTPException(
             status_code=404, detail="Book not found"
         )
-    return {"id": book_id, **books_db[book_id].dict()}
+    return {"id": book_id, "title": book.title, "year": book.year}
