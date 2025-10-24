0ec9e42a009b4b3676e3c0c807edb2269fe3e957
Alexandr <khsashko@yandex.ru>
2025-10-24 12:17:07 +0300

    TASK-439: Задача - AsyncEngine и AsyncSession

--- app/database.py
+++ app/database.py
@@ -1,4 +1,12 @@
 from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+
+
+engine = create_async_engine(
+    "postgresql+asyncpg://fastapi_user:mysecretpassword@localhost:5432"
+    "/fastapi")
+
+session_maker = async_sessionmaker(engine, expire_on_commit=False)
 
 
 class Base(DeclarativeBase):
