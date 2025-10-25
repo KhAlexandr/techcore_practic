c1da5d57eb6b832a78ca5aab624436c6e3dbaa1b
Alexandr <khsashko@yandex.ru>
2025-10-25 07:32:17 +0300

    TASK-441: Применил black для читаемости

--- app/routers/books.py
+++ app/routers/books.py
@@ -21,43 +21,36 @@ router = APIRouter(prefix="/books", tags=["Упрпвление книгами"]
 
 
 class BookRepository:
-    async def get_by_id(
-            self,
-            book_id: int,
-            session: AsyncSession
-    ):
+    async def get_by_id(self, book_id: int, session: AsyncSession):
         result = await session.execute(
-            select(Book)
-            .options(selectinload(Book.author))
-            .where(Book.id == book_id))
+            select(Book).options(selectinload(Book.author)).where(Book.id == book_id)
+        )
         return result.scalar_one_or_none()
 
     async def create(
-            self, title: str,
-            session: AsyncSession,
-            year: int | None = None,
+        self,
+        title: str,
+        author_id: int,
+        session: AsyncSession,
+        year: int | None = None,
     ) -> Book:
-        book = Book(title=title, year=year)
+        book = Book(title=title, year=year, author_id=author_id)
         session.add(book)
         await session.commit()
         await session.refresh(book)
         return book
 
     async def create_author_and_book(
-            self,
-            first_name: str,
-            last_name: str,
-            title: str,
-            session: AsyncSession,
-            age: int | None = None,
-            year: int | None = None
+        self,
+        first_name: str,
+        last_name: str,
+        title: str,
+        session: AsyncSession,
+        age: int | None = None,
+        year: int | None = None,
     ):
         async with session.begin():
-            author = Author(
-                first_name=first_name,
-                last_name=last_name,
-                age=age
-            )
+            author = Author(first_name=first_name, last_name=last_name, age=age)
             session.add(author)
             await session.flush()
 
@@ -70,9 +63,12 @@ book_repo = BookRepository()
 
 
 @router.post("/")
-async def create_book(book: BookScheme, session: AsyncSession = Depends(get_db_session)):
-    new_book = await book_repo.create(title=book.title, year=book.year,
-                                      session=session)
+async def create_book(
+    book: BookScheme, author_id: int, session: AsyncSession = Depends(get_db_session)
+):
+    new_book = await book_repo.create(
+        title=book.title, year=book.year, author_id=author_id, session=session
+    )
     return {"id": new_book.id, "title": new_book.title, "year": new_book.year}
 
 
@@ -80,7 +76,5 @@ async def create_book(book: BookScheme, session: AsyncSession = Depends(get_db_s
 async def get_book(book_id: int, session: AsyncSession = Depends(get_db_session)):
     book = await book_repo.get_by_id(book_id, session=session)
     if not book:
-        raise HTTPException(
-            status_code=404, detail="Book not found"
-        )
+        raise HTTPException(status_code=404, detail="Book not found")
     return {"id": book_id, "title": book.title, "year": book.year}
