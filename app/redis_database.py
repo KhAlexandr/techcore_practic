d7bb996599351bc1d5d64ddbc4921f2c7773edfe
Alexandr <khsashko@yandex.ru>
2025-10-25 07:32:01 +0300

    TASK-441: Задача - redis-py (Async)

---
+++ app/redis_database.py
@@ -0,0 +1,20 @@
+import redis.asyncio as redis
+
+import asyncio
+
+
+redis_client = redis.Redis(host="localhost", port=6379, decode_responses=True)
+
+
+async def ping_redis():
+    result = await redis_client.ping()
+    return result
+
+
+async def main():
+    result = await ping_redis()
+    print(result)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
