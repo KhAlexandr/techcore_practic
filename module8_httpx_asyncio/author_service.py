56680b9080fc5e0ce5e109923c648e64b81201f3
Alexandr <khsashko@yandex.ru>
2025-10-27 11:18:57 +0300

    TASK-445: Задача - Backoff (Retry Policy)

--- module8_httpx_asyncio/author_service.py
+++ module8_httpx_asyncio/author_service.py
@@ -2,26 +2,30 @@ import httpx
 
 import asyncio
 
+import backoff
+
 
 class AuthorService:
     def __init__(self):
-        self.client = httpx.AsyncClient()
+        self.client = None
 
+    async def __aenter__(self):
+        self.client = httpx.AsyncClient()
+        return self
+
+    async def __aexit__(self, exc_type, exc_val, exc_tb):
+        await self.client.aclose()
+
+    @backoff.on_exception(backoff.expo, httpx.RequestError, max_tries=3)
     async def get_url(self, url):
         response = await asyncio.wait_for(self.client.get(url), timeout=2.0)
         return response
 
 
-author_repo = AuthorService()
-
-
-async def get_response(url):
-    response = await author_repo.get_url(url)
-    print(response.json())
-
-
 async def main():
-    await get_response("https://httpbin.org/get")
+    async with AuthorService() as author_repo:
+        response = await author_repo.get_url("https://httpbin.org/get")
+        print(response.json())
 
 
 if __name__ == "__main__":
