fbc8e2085befaae6157f4b1e8fb460d7a9674f66
Alexandr <khsashko@yandex.ru>
2025-10-27 15:42:37 +0300

    TASK-445: Задача - gRPC (Python)

---
+++ module8_httpx_asyncio/client.py
@@ -0,0 +1,15 @@
+import grpc
+import asyncio
+import greeter_pb2
+import greeter_pb2_grpc
+
+
+async def run():
+    async with grpc.aio.insecure_channel('localhost:50051') as channel:
+        stub = greeter_pb2_grpc.GreeterStub(channel)
+        response = await stub.SayHello(greeter_pb2.HelloRequest(name="Александр"))
+        print("Ответ:", response.message)
+
+
+if __name__ == '__main__':
+    asyncio.run(run())
