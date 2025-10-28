9222d46d11488f475c870bc4a4ad57366d5d6192
Alexandr <khsashko@yandex.ru>
2025-10-28 14:47:53 +0300

    TASK-447: Задача - Celery Beat (Планировщик)

--- app/celery_tasks/celery_apps.py
+++ app/celery_tasks/celery_apps.py
@@ -13,4 +13,18 @@ celery_app = Celery(
     backend="redis://redis:6379/0",
 )
 
+
+@celery_app.task
+def nightly_report():
+    print("Nightly report executed!")
+    return "Report completed"
+
+
+celery_app.conf.beat_schedule = {
+    'nightly-report': {
+        'task': 'app.celery_tasks.celery_apps.nightly_report',
+        'schedule': 300.0,
+    },
+}
+
 import app.celery_tasks.worker_service
