e6c6f9cf66b8df0815e325c377197dae74394e6d
Alexandr <khsashko@yandex.ru>
2025-10-28 14:04:07 +0300

    TASK-447: Задача -Celery Retry (Повторы)

--- app/celery_tasks/worker_service.py
+++ app/celery_tasks/worker_service.py
@@ -11,8 +11,11 @@ router = APIRouter(prefix="/order", tags=["Тестирование celery"])
 class WorkerService:
     @celery_app.task(bind=True)
     def process_order(self, order_id):
-        time.sleep(10)
-        return order_id
+        try:
+            time.sleep(10)
+            return order_id
+        except Exception as exc:
+            raise self.retry(exc=exc, countdown=5, max_retries=3)
 
 
 worker = WorkerService()
