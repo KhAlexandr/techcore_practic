c1f00ea344e9b9683b8cd416dd53b32f679bba56
Alexandr <khsashko@yandex.ru>
2025-10-31 10:55:29 +0300

    TASK-451: Задача - Gateway (Агрегация запросов)

--- app/gateway.py
+++ app/gateway.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, Depends
 import httpx
+import asyncio
 
 from app.auth import verify_user
 
@@ -16,5 +17,21 @@ async def get_book(book_id: int, user: dict = Depends(verify_user)):
                 f"http://api:80/api/books/{book_id}", timeout=10.0, headers=headers
             )
             return response.json()
+        except Exception as e:
+            return {"error": f"Ошибка Gateway: {str(e)}"}
+
+
+@app.get("/details/{id}")
+async def get_details(id: int):
+    async with httpx.AsyncClient() as client:
+        try:
+            task1 = asyncio.create_task(
+                client.get(f"http://api:80/api/books/{id}", timeout=10.0,)
+            )
+            task2 = asyncio.create_task(
+                client.get(f"http://api:80/api/api/products/{id}/reviews")
+            )
+            book, review = await asyncio.gather(task1, task2)
+            return {"book": book.json(), "review": review.json()}
         except Exception as e:
             return {"error": f"Ошибка Gateway: {str(e)}"}
