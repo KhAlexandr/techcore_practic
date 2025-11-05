2d12d7b83c29d193f7f204410a220ec61ba038ef
Alexandr Khachaturian <khsashko@yandex.ru>
2025-11-05 19:18:52 +0300

    TASK-457: Задача - Health Checks (liveness/readiness)

--- app/main.py
+++ app/main.py
@@ -68,6 +68,11 @@ async def hello():
     return {"message": "Hello, World"}
 
 
+@app.get("/healthz")
+async def health_check():
+    return {"status": "healthy"}
+
+
 @app.get("/metrics")
 async def metrics_endpoint():
     return Response(generate_latest(), media_type=CONTENT_TYPE_LATEST)
